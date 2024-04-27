f9b5fa284571ab1c77ec27da5813ecd92b19587b
Joel Thomas <joelthomas1103@gmail.com>
2024-04-27 11:56:49 +0530

    final commit

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("",views.home,name="home"),
-    path("get-route",views.get_route,name="get-route")
+    path("get-route",views.get_route,name="get-route"),
+    path("preview",views.preview,name="preview")
 ]
--- home/views.py
+++ home/views.py
@@ -7,15 +7,16 @@ from . Utils import insert_nodes, find_shortest_path, generate_directions,draw_p
 from . models import *
 from django.core.files.base import ContentFile
 import base64
+import os
+from django.conf import settings
 
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    return render(request, 'home.html')
 
-import os
-from django.conf import settings
+
 
 def get_route(request):
     # Your existing code to generate image and directions
@@ -59,3 +60,48 @@ def get_route(request):
 
     # Return JSON response
     return JsonResponse(response_data)
+
+
+
+def preview(request):
+    if request.method == "GET":
+        start_node_name = request.GET.get('start_node_name')
+        end_node_name = request.GET.get('end_node_name')
+    image_path = r'static\assets\image.jpg'  
+    node_coordinates = [(53, 259),(53, 306),(64, 347),(165, 347),(206, 347),(248, 326),(306, 327),(156, 220),
+    (199, 225),(370, 209),(391, 184),(415, 211),(513, 325),(559, 324),(629, 345),(595, 345),
+    (737, 344),(754, 314),(753, 254),(628, 225),(589, 226),(158, 271),(251, 271),(330, 273),
+    (387, 248),(425, 275),(527, 274),(589, 268),(670, 281),(733, 278)]  
+    node_names = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','P','Q','R','S','T','U','V','X','Y','Z','A1','B3','C1','D2','E1','F1']
+
+    img = insert_nodes(image_path, node_coordinates, node_names)
+
+    # Your existing code to find shortest path and generate directions
+    
+    print(start_node_name)
+    print(end_node_name)
+    shortest_path_indices = find_shortest_path(node_coordinates, str(start_node_name).upper(), str(end_node_name).upper())
+    directions = generate_directions(shortest_path_indices, node_coordinates, node_names)
+
+    # Draw the path on the image
+    draw_path(img, shortest_path_indices, node_coordinates)
+
+    # Serialize the image data
+    _, img_encoded = cv2.imencode('.jpg', img)
+    img_base64 = base64.b64encode(img_encoded).decode('utf-8')
+
+    # Save the image data to the static files directory
+    image_filename = f'navigation_image_{start_node_name}_{end_node_name}.jpg'  # Unique filename
+    image_path = os.path.join(r"static", 'navigation_images', image_filename)
+    with open(image_path, 'wb') as f:
+        f.write(img_encoded.tobytes())
+
+    # Print directions
+    for direction in directions:
+        print(direction)
+    response_data = {
+        "directions": directions,
+        "image_filename": image_filename  # Include the image filename in the response
+    }
+
+    return render(request, "preview.html", response_data)
